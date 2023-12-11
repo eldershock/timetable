b8e56da0289297f19fd95adc52aef493247fd4ac
Kresh195 <KreshF5@mail.ru>
2023-12-11 19:59:19 +0700

    Fixed some syntax

--- globalSegment.py
+++ globalSegment.py
@@ -2,29 +2,34 @@ import string
 import re
 import os
 
+
 symbols = string.punctuation
 studentsWorkDaysTime = []
 
+
 def clear():
     return os.system("cls")
+
 
 #error checkers
 def strHasOnlyLetters(text: str) -> bool:
     for i in text:
         if i.isdigit():
-            print("Ошибка: в строке присутсвуют цифры.")
+            print("Ошибка: в строке присутствуют цифры.")
             return False
         elif i in symbols:
-            print("Ошибка: в строке присутсвуют посторонние символы.")
+            print("Ошибка: в строке присутствуют посторонние символы.")
             return False
     return True
+
 
 def strIsInt(text: str) -> bool:
     if text.isdigit():
         return True
-    print("Ошибка: число содержить посторонние символы.")
+    print("Ошибка: число содержит посторонние символы.")
     return False
-        
+
+
 def dateTimeValidate(durationTime) -> bool:
     if re.match("\\d{2}:\\d{2}-\\d{2}:\\d{2}", durationTime) or durationTime == '0':
         return True
